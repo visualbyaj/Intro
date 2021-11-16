9bbf314bfc3a5832d6988884b16b37ba09ad4324
visualbyaj <75160589+visualbyaj@users.noreply.github.com>
2021-11-15 20:15:07 -0500

    Update input.py

--- input.py
+++ input.py
@@ -1,5 +1,5 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
+print('Hello! My name is AJ.')
+print('I was created in 2021.')
 print('Please, remind me your name.')
 username = input()
 print('What a great name you have', username !)
